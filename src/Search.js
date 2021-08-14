4cc8112bfadf3656452933f91c7b1a27a81d48b0
sofiapaiva90 <73502292+sofiapaiva90@users.noreply.github.com>
2021-08-14 19:59:57 +0100

    Added the word meanings

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
 let[word, setWord] = useState("");
+let[results, setResults] = useState(null);
 
 function handleResponse(response) {
 console.log(response.data);
+setResults(response.data[0]);
 }
 
 function search (event) {
@@ -28,6 +31,7 @@ function handleWordChange(event) {
                 <input type="search" onChange={handleWordChange} placeholder="Type a word..." className="form-control"/>
                 <i className="fas fa-search"></i> 
             </form>
+            <Results results={results}/>
         </div>
     )
 }
