753a37938a7ec8322e8b69230aa5ee88709f0ec5
sofiapaiva90 <73502292+sofiapaiva90@users.noreply.github.com>
2021-08-16 15:27:10 +0100

    react and api corrections

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,9 @@ import ReactAudioPlayer from 'react-audio-player';
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
+    if (props.phonetic.audio) {
 return (
+    
     <div className="Phonetic">
         <div className="text">{props.phonetic.text}</div>
     <ReactAudioPlayer
@@ -13,4 +15,9 @@ return (
 />
 </div>
 );
+}
+
+else {
+    return null;
+}
 }
